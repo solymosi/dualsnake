c147666ec332cfe55ccf62238319389319c553a9
Mate Solymosi <mate@solymosi.eu>
2011-03-31 20:23:44 +0200

    Full-window status messages

--- DualSnake/MainForm.Designer.cs
+++ DualSnake/MainForm.Designer.cs
@@ -28,19 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.AgainPanel = new System.Windows.Forms.Panel();
             this.AgainButton = new System.Windows.Forms.Button();
+            this.AgainPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusLabel.BackColor = System.Drawing.Color.Black;
+            this.StatusLabel.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.StatusLabel.Location = new System.Drawing.Point(0, 231);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(546, 86);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AgainPanel
+            // 
+            this.AgainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.AgainPanel.Controls.Add(this.AgainButton);
+            this.AgainPanel.Location = new System.Drawing.Point(5, 515);
+            this.AgainPanel.Name = "AgainPanel";
+            this.AgainPanel.Size = new System.Drawing.Size(182, 60);
+            this.AgainPanel.TabIndex = 2;
             // 
             // AgainButton
             // 
             this.AgainButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.AgainButton.BackColor = System.Drawing.Color.Maroon;
+            this.AgainButton.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.AgainButton.ForeColor = System.Drawing.Color.White;
-            this.AgainButton.Location = new System.Drawing.Point(443, 566);
+            this.AgainButton.Location = new System.Drawing.Point(24, 11);
             this.AgainButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.AgainButton.Name = "AgainButton";
-            this.AgainButton.Size = new System.Drawing.Size(103, 35);
-            this.AgainButton.TabIndex = 0;
+            this.AgainButton.Size = new System.Drawing.Size(133, 40);
+            this.AgainButton.TabIndex = 1;
             this.AgainButton.Text = "Again?";
             this.AgainButton.UseVisualStyleBackColor = false;
             this.AgainButton.Click += new System.EventHandler(this.AgainButton_Click);
@@ -50,8 +75,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(545, 600);
-            this.Controls.Add(this.AgainButton);
+            this.ClientSize = new System.Drawing.Size(545, 575);
+            this.Controls.Add(this.AgainPanel);
+            this.Controls.Add(this.StatusLabel);
             this.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -70,12 +96,15 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyUp);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.AgainPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Panel AgainPanel;
         private System.Windows.Forms.Button AgainButton;
 
 
--- DualSnake/MainForm.cs
+++ DualSnake/MainForm.cs
@@ -33,8 +33,13 @@ namespace DualSnake
         const int BlockWidth = 70;
         const int BlockHeight = 40;
         const int BlockDisplaySize = 10;
+
+        Color ErrorColor = Color.Maroon;
+        Color WarningColor = Color.FromArgb(128, 64, 0);
+        Color SuccessColor = Color.Green;
+
+        bool Draw = false;
         
-        string Status = "";
         string ConnectTo = "";
 
         public MainForm()
@@ -44,32 +49,41 @@ namespace DualSnake
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            this.ClientSize = new Size(BlockWidth * BlockDisplaySize, BlockHeight * BlockDisplaySize + 35);
+            this.ClientSize = new Size(BlockWidth * BlockDisplaySize, BlockHeight * BlockDisplaySize + 60);
+            this.Left = Screen.PrimaryScreen.WorkingArea.Width / 2 - this.Width / 2;
+            this.Top = Screen.PrimaryScreen.WorkingArea.Height / 2 - this.Height / 2;
             this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
-            AgainButton.Visible = false;
+            AgainPanel.Visible = false;
+            AgainPanel.Top = this.ClientRectangle.Height - AgainPanel.Height;
+            AgainPanel.Left = 0;
+            AgainPanel.Width = this.Width;
             AgainButton.Enabled = false;
+            AgainButton.Left = this.Width / 2 - AgainButton.Width / 2;
+            StatusLabel.Left = 0;
+            StatusLabel.Width = this.Width;
+            StatusLabel.Top = BlockHeight * BlockDisplaySize / 2 - StatusLabel.Height / 2;
             ConnectTo = (string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Solymosi\DualSnake", "LastIP", "");
             PopupConnectionDialog();
         }
 
         private void Connect()
         {
-            AgainButton.Visible = false;
+            AgainPanel.Visible = false;
             AgainButton.Enabled = false;
+            Draw = false;
             Server = new Client();
             Server.Socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
-            SetStatus("Connecting to server...");
+            SetStatus("Connecting to " + ConnectTo + "...");
             Server.Connect(ConnectTo, 1991);
             Server.Received += new Client.ReceiveDelegate(Server_Received);
             Server.Connected += new Client.ConnectDelegate(delegate
             {
-                SetStatus("Connected to server. Waiting for an opponent to join...");
-                this.Invoke((MethodInvoker)delegate { AgainButton.Visible = false; AgainButton.Enabled = false; });
+                this.Invoke((MethodInvoker)delegate { AgainPanel.Visible = false; AgainButton.Enabled = false; });
             });
             Server.Closed += new Client.CloseDelegate(delegate(object o, Client.CloseEventArgs ea)
             {
-                if (ea.Type == Client.CloseType.Dropped && !GameOver) { SetStatus("The server has dropped the connection :("); }
-                this.Invoke((MethodInvoker)delegate { AgainButton.Visible = true; AgainButton.Enabled = true; });
+                if (ea.Type == Client.CloseType.Dropped && !GameOver) { SetStatus("Connection failed", ErrorColor); }
+                this.Invoke((MethodInvoker)delegate { AgainPanel.Visible = true; AgainButton.Enabled = true; });
             });
         }
 
@@ -89,10 +103,20 @@ namespace DualSnake
             }
         }
 
-        private void SetStatus(string Message)
+        private void SetStatus(string Message) { SetStatus(Message, Color.Black); }
+        private void SetStatus(string Message, Color Color)
         {
-            Status = Message;
-            RePaint();
+            if (this.InvokeRequired) { this.Invoke((MethodInvoker)delegate { this.SetStatus(Message, Color); }); return; }
+            StatusLabel.Visible = true;
+            StatusLabel.Text = Message;
+            StatusLabel.BackColor = Color;
+            this.Refresh();
+        }
+
+        private void ClearStatus()
+        {
+            if (this.InvokeRequired) { this.Invoke((MethodInvoker)delegate { this.ClearStatus(); }); return; }
+            StatusLabel.Visible = false;
         }
 
         private void RePaint()
@@ -106,30 +130,36 @@ namespace DualSnake
             if (e.Text == "#First")
             {
                 Me = 1;
-                SetStatus("Connected to server. Waiting for an opponent to join...");
+                SetStatus("Waiting for an opponent...");
             }
 
             if (e.Text == "#Second")
             {
                 Me = 2;
-                SetStatus("Connected to server. Game will start now...");
             }
 
             if (e.Text.StartsWith("#Countdown "))
             {
-                SetStatus("The game will start in " + e.Text.Substring(11) + " seconds...");
+                SetStatus("Starting game...", WarningColor);
             }
 
             if (e.Text == "#Draw")
             {
                 GameOver = true;
-                SetStatus("Game over! It's a DRAW! :S");
+                SetStatus("It's a DRAW!", WarningColor);
             }
 
             if (e.Text.StartsWith("#Winner "))
             {
                 GameOver = true;
-                SetStatus("Game over! You " + (int.Parse(e.Text.Substring(8)) == Me ? "WON :)" : "LOST :("));
+                if (int.Parse(e.Text.Substring(8)) == Me)
+                {
+                    SetStatus("You're the WINNER!", SuccessColor);
+                }
+                else
+                {
+                    SetStatus("You LOST!", ErrorColor);
+                }
             }
 
             if (e.Text.StartsWith("#Status "))
@@ -141,8 +171,8 @@ namespace DualSnake
                 SnakeTwo = FromRepresentation(pqq[3]);
                 TurboEnabled = pqq[4] == "E";
                 TurboCounter = int.Parse(pqq[5]);
-
-                SetStatus("[TURBO]");
+                ClearStatus();
+                Draw = true;
                 RePaint();
             }
         }
@@ -160,6 +190,11 @@ namespace DualSnake
 
         private void MainForm_Paint(object sender, PaintEventArgs e)
         {
+            if (!Draw)
+            {
+                e.Graphics.Clear(Color.Black);
+                return;
+            }
             Graphics GFX = e.Graphics;
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
             e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Low;
@@ -187,17 +222,10 @@ namespace DualSnake
                 GFX.FillRectangle(Both ? new TextureBrush(Resources.Both) : (Me == 2 ? new TextureBrush(Resources.Me) : new TextureBrush(Resources.Them)), new Rectangle(BlockDisplaySize * (SnakeTwo[i].X - 1), BlockDisplaySize * (SnakeTwo[i].Y - 1), BlockDisplaySize, BlockDisplaySize));
             }
 
-            if (Status == "[TURBO]")
-            {
-                GFX.DrawImage(Resources.TurboIcon, new PointF(15, BlockHeight * BlockDisplaySize + 3));
-                GFX.DrawRectangle(new Pen(Color.FromArgb(170, 106, 0)), new Rectangle(50, BlockHeight * BlockDisplaySize + 8, 201, 21));
-                GFX.FillRectangle(new SolidBrush(Color.FromArgb(50, 50, 50)), new Rectangle(51, BlockHeight * BlockDisplaySize + 9, 200, 20));
-                GFX.DrawImageUnscaledAndClipped(Resources.PowerMeter, new Rectangle(51, BlockHeight * BlockDisplaySize + 9, TurboCounter * 2, 20));
-            }
-            else
-            {
-                GFX.DrawString(Status, new Font(new FontFamily("trebuchet ms"), 9, FontStyle.Bold), Brushes.White, new PointF(15, BlockHeight * BlockDisplaySize + 8));
-            }
+            GFX.DrawImage(Resources.TurboIcon, new PointF(15, BlockHeight * BlockDisplaySize + 3));
+            GFX.DrawRectangle(new Pen(Color.FromArgb(170, 106, 0)), new Rectangle(50, BlockHeight * BlockDisplaySize + 8, 201, 21));
+            GFX.FillRectangle(new SolidBrush(Color.FromArgb(50, 50, 50)), new Rectangle(51, BlockHeight * BlockDisplaySize + 9, 200, 20));
+            GFX.DrawImageUnscaledAndClipped(Resources.PowerMeter, new Rectangle(51, BlockHeight * BlockDisplaySize + 9, TurboCounter * 2, 20));
         }
 
         private void MainForm_KeyDown(object sender, KeyEventArgs e)

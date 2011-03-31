1280d2e8cb95f77cb826d2a6227670241f5c8b1c
Mate Solymosi <mate@solymosi.eu>
2011-03-31 12:55:56 +0200

    Fixed again? button being activated when hidden

--- DualSnake/MainForm.cs
+++ DualSnake/MainForm.cs
@@ -41,12 +41,14 @@ namespace DualSnake
             this.ClientSize = new Size(BlockWidth * BlockDisplaySize, BlockHeight * BlockDisplaySize + 35);
             this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
             AgainButton.Visible = false;
+            AgainButton.Enabled = false;
             PopupConnectionDialog();
         }
 
         private void Connect()
         {
             AgainButton.Visible = false;
+            AgainButton.Enabled = false;
             Server = new Client();
             SetStatus("Connecting to server...");
             Server.Connect(ConnectTo, 1991);
@@ -54,12 +56,12 @@ namespace DualSnake
             Server.Connected += new Client.ConnectDelegate(delegate
             {
                 SetStatus("Connected to server. Waiting for an opponent to join...");
-                this.Invoke((MethodInvoker)delegate { AgainButton.Visible = false; });
+                this.Invoke((MethodInvoker)delegate { AgainButton.Visible = false; AgainButton.Enabled = false; });
             });
             Server.Closed += new Client.CloseDelegate(delegate(object o, Client.CloseEventArgs ea)
             {
                 if (ea.Type == Client.CloseType.Dropped && !GameOver) { SetStatus("The server has dropped the connection :("); }
-                this.Invoke((MethodInvoker)delegate { AgainButton.Visible = true; });
+                this.Invoke((MethodInvoker)delegate { AgainButton.Visible = true; AgainButton.Enabled = true; });
             });
         }
 
